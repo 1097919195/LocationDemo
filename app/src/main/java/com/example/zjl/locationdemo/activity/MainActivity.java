237e788ce688e00ad96065276c27f37f53b88097
zjl7577 <1097919195@qq.com>
2018-05-23 17:10:30 +0800

    baseDemo

---
+++ app/src/main/java/com/example/zjl/locationdemo/activity/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.zjl.locationdemo.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.baidu.location.BDLocation;
+import com.baidu.location.LocationClientOption;
+import com.example.zjl.locationdemo.R;
+import com.example.zjl.locationdemo.app.AppApplication;
+import com.example.zjl.locationdemo.baiduSDK.MyLocationListener;
+import com.jaydenxiao.common.base.BaseActivity;
+import com.jaydenxiao.common.commonutils.LogUtils;
+
+import butterknife.BindView;
+
+public class MainActivity extends BaseActivity {
+
+    @BindView(R.id.start_location)
+    Button start_location;
+    @BindView(R.id.stop_location)
+    Button stop_location;
+    @BindView(R.id.clear)
+    Button clear;
+    @BindView(R.id.display_info)
+    TextView display_info;
+
+    BDLocation location;
+    @Override
+    public int getLayoutId() {
+        return R.layout.act_main;
+    }
+
+    @Override
+    public void initPresenter() {
+
+    }
+
+    @Override
+    public void initView() {
+        startLocation();
+    }
+
+    private void startLocation() {
+        start_location.setOnClickListener(v -> {
+            AppApplication.getmLocationClient().start();
+        });
+
+        stop_location.setOnClickListener(v -> {
+            AppApplication.getmLocationClient().stop();
+
+            location = AppApplication.getmLocationClient().getLastKnownLocation();//放在start后面，防止还没有获取到BDLocation的情况
+            if (location != null) {
+                display_info.post(() ->
+                        display_info.setText(location.getAddrStr())
+                );
+            }
+        });
+
+        clear.setOnClickListener(v -> {
+            display_info.setText("");
+        });
+    }
+}
---
+++ app/src/main/java/com/example/zjl/locationdemo/app/AppApplication.java
@@ -0,0 +1,85 @@
+package com.example.zjl.locationdemo.app;
+
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.example.zjl.locationdemo.BuildConfig;
+import com.example.zjl.locationdemo.baiduSDK.MyLocationListener;
+import com.jaydenxiao.common.baseapp.BaseApplication;
+import com.jaydenxiao.common.commonutils.LogUtils;
+
+/**
+ * Created by Administrator on 2018/5/23 0023.
+ */
+
+public class AppApplication extends BaseApplication {
+    public static LocationClient mLocationClient = null;
+    private MyLocationListener myListener = new MyLocationListener();
+    LocationClientOption option = new LocationClientOption();
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        LogUtils.logInit(BuildConfig.LOG_DEBUG);
+
+        mLocationClient = new LocationClient(getApplicationContext());
+        //声明LocationClient类
+        mLocationClient.registerLocationListener(myListener);
+        //注册监听函数
+
+
+        initLocation();
+    }
+
+    public static LocationClient getmLocationClient() {
+        return mLocationClient;
+    }
+
+    private void initLocation() {
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);
+//可选，设置定位模式，默认高精度
+//LocationMode.Hight_Accuracy：高精度；
+//LocationMode. Battery_Saving：低功耗；
+//LocationMode. Device_Sensors：仅使用设备；
+
+        option.setCoorType("bd09ll");
+//可选，设置返回经纬度坐标类型，默认gcj02
+//gcj02：国测局坐标；
+//bd09ll：百度经纬度坐标；
+//bd09：百度墨卡托坐标；
+//海外地区定位，无需设置坐标类型，统一返回wgs84类型坐标
+
+        option.setScanSpan(1000);
+//可选，设置发起定位请求的间隔，int类型，单位ms
+//如果设置为0，则代表单次定位，即仅定位一次，默认为0
+//如果设置非0，需设置1000ms以上才有效
+
+        option.setOpenGps(true);
+//可选，设置是否使用gps，默认false
+//使用高精度和仅用设备两种定位模式的，参数必须设置为true
+
+        option.setLocationNotify(true);
+//可选，设置是否当GPS有效时按照1S/1次频率输出GPS结果，默认false
+
+        option.setIgnoreKillProcess(false);
+//可选，定位SDK内部是一个service，并放到了独立进程。
+//设置是否在stop的时候杀死这个进程，默认（建议）不杀死，即setIgnoreKillProcess(true)
+
+        option.SetIgnoreCacheException(false);
+//可选，设置是否收集Crash信息，默认收集，即参数为false
+
+        option.setWifiCacheTimeOut(5*60*1000);
+//可选，7.2版本新增能力
+//如果设置了该接口，首次启动定位时，会先判断当前WiFi是否超出有效期，若超出有效期，会先重新扫描WiFi，然后定位
+
+        option.setEnableSimulateGps(false);
+//可选，设置是否需要过滤GPS仿真结果，默认需要，即参数为false
+
+        option.setIsNeedAddress(true);
+//可选，是否需要地址信息，默认为不需要，即参数为false
+//如果开发者需要获得当前点的地址信息，此处必须为true
+
+        mLocationClient.setLocOption(option);
+//mLocationClient为第二步初始化过的LocationClient对象
+//需将配置好的LocationClientOption对象，通过setLocOption方法传递给LocationClient对象使用
+//更多LocationClientOption的配置，请参照类参考中LocationClientOption类的详细说明
+    }
+}
